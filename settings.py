340da7211f159ddd6ba94182426fde7a6455cb31
lmaobadatmath <abhinav.sasikumar@gmail.com>
2021-04-26 20:18:04 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,5 +2,4 @@ import os
 
 DISCORD_TOKEN="TOKEN" #insert TOKEN here instead of TOKEN
 API_KEY="PERSPECTIVEKEY" #insert your perspective API key here instead of PERSPECTIVEKEY
-Queue_guild_ID="GUILD" #replace GUILD with the guild id you want the modqueue to appear in
-Queue_channel_ID="CHANNEL" #replace CHANNEL with the channel id you want the modqueue to appear in
+
