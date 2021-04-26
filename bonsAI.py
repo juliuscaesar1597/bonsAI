94f9ac3377fbcd05664565ce87deedb80b408368
lmaobadatmath <abhinav.sasikumar@gmail.com>
2021-04-26 14:53:10 -0400

    added license

--- bonsAI.py
+++ bonsAI.py
@@ -1,3 +1,18 @@
+# bonsAI Discord Moderation Bot using Artificial Intelligence
+# Copyright (C) 2021 lmaobadatmath
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.# Byte-compiled / optimized / DLL files
 import discord, json, requests
 from ratelimit import limits
 import string
