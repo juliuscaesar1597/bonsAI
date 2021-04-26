5441b63d11eeb0c1b2fcf8c9a9a101d2ee02b4f1
lmaobadatmath <abhinav.sasikumar@gmail.com>
2021-04-26 15:04:59 -0400

    license issues

--- bonsAI.py
+++ bonsAI.py
@@ -12,7 +12,7 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.# Byte-compiled / optimized / DLL files
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import discord, json, requests
 from ratelimit import limits
 import string
